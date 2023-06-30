35af62effd640200a16c57f89ae9b1f95129d69c
Lawrence <82725076+la-jig@users.noreply.github.com>
2023-06-30 14:35:27 -0400

    Add Source

---
+++ Example-Bundle/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Example-Bundle
+//
+//  Created by Lawrence Williams on 6/30/23.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
